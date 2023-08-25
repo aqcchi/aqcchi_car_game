fa31c98aeb2815199d85c6905047e3eeca3dbcea
Vaya Line <134380865+aqcchi@users.noreply.github.com>
2023-08-25 14:36:26 +0300

    Update car_game.py

--- car_game.py
+++ car_game.py
@@ -11,22 +11,22 @@ speed = 1
 
 pygame.init()
 running = True
-#set window size
+# set window size
 screen = pygame.display.set_mode(size)
-#set title
+# set title
 pygame.display.set_caption("Anelia's car game")
 # set background color
 screen.fill((60, 220, 0))
 
-#apply changes
+# apply changes
 pygame.display.update()
 
-#load player vehicle
+# load player vehicle
 car = pygame.image.load("my_car_resized.png")
 car_loc = car.get_rect()
 car_loc.center = right_lane, height*0.8
 
-#load enemy vehicle
+# load enemy vehicle
 car2 = pygame.image.load("enemy_car_resized.png")
 car2_loc = car.get_rect()
 car2_loc.center = left_lane, height*0.2
@@ -35,17 +35,22 @@ counter = 0
 
 # game loop
 while running:
+    
     counter += 1
+    
     if counter == 5000:
         speed += 0.15
         counter = 0
-        print("level up", speed)
+        print("level up", speed)  
+        
     # animate enemy vehicle
     car2_loc[1] += speed
 
-    if car2_loc[1] > height:
+    if car2_loc[1] > height: 
+        
         if random.randint(0,1) == 0:
             car2_loc.center = right_lane, -200
+            
         else:
             car2_loc.center = left_lane, -200
 
@@ -55,12 +60,15 @@ while running:
 
     # event listeners
     for event in pygame.event.get():
+        
         if event.type == QUIT:
             running = False
 
         if event.type == KEYDOWN:
+            
             if event.key in [K_a, K_LEFT]:
                 car_loc = car_loc.move([-int(road_w/2), 0])
+                
             if event.key in [K_d, K_RIGHT]:
                 car_loc = car_loc.move([int(road_w / 2), 0])
 
@@ -87,6 +95,5 @@ while running:
     screen.blit(car, car_loc)
     screen.blit(car2, car2_loc)
     pygame.display.update()
-
 
 pygame.quit()
